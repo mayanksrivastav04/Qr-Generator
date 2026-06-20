70fc566702081b3ed8155c27a358f46b798fe548
Mayank Srivastav <mastermayank786@gmail.com>
2026-06-20 23:41:24 +0530

    Updated UI and responsiveness

--- src/components/FullQr.jsx
+++ src/components/FullQr.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import Qrgenerator from './Qrgenerator'
+import Qrgenerator from './QRgenerator'
 
 const FullQr = () => {
   return (
     <div className="border-amber-500 p-5 rounded-2xl w-80 lg:w-[200] border-4 flex flex-col justify-center items-center">
         <h1 className="text-2xl font-bold mb-4 uppercase">QR code Generator</h1>
-        <Qrgenerator/>
+        <QRgenerator/>
     </div>
   )
 }
